c44ecb9b46dd399c174b7bf858f8ac25ef79d169
knnv-ar <106931704+knnv-ar@users.noreply.github.com>
2023-03-30 10:15:47 -0300

    Update: Info Modal Component completed

--- components/Billboard.tsx
+++ components/Billboard.tsx
@@ -1,11 +1,18 @@
 import useBillboard from "@/hooks/useBillboard";
-import React from "react";
+import useInfoModal from "@/hooks/useInfoModal";
+import React, { useCallback } from "react";
 
 import { AiOutlineInfoCircle } from "react-icons/ai";
 import PlayButton from "./PlayButton";
 
 const Billboard = () => {
   const { data } = useBillboard();
+  const { openModal } = useInfoModal();
+
+  const handleOpenModal = useCallback(() => {
+    openModal(data?.id);
+  }, [openModal, data?.id]);
+
   return  (
     <div className="relative h-[56.25vw]">
       <video
@@ -52,6 +59,7 @@ const Billboard = () => {
           <div className="flex flex-row items-center mt-3 md:mt-4 gap-3">
             <PlayButton movieId={data?.id}/>
             <button
+            onClick={handleOpenModal}
               className="
                 bg-white
                 text-white
